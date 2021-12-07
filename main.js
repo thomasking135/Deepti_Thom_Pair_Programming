8af143a9667ab4f5f341d55f561caaf7a5ed3691
Tom E King <62085061+thomasking135@users.noreply.github.com>
2021-12-08 12:27:49 +1300

    Files uploaded for the pair activity with Deepti.
    The files uploaded included the index.html, main.css, and main.js files. This will allow us to build a small project together.

---
+++ main.js
@@ -0,0 +1,6 @@
+//This file will contain our JS functions
+
+//It might be a link to something we are passionate about
+
+//Or it might be a button that performs an operation, i.e. performing a task
+
