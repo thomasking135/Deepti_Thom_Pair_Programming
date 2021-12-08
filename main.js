b57ab44656d347f621d12d39915bd1887f3e5151
thomasking135 <thomasking135@gmail.com>
2021-12-08 16:09:46 +1300

    Updated css, js and index files.

--- main.js
+++ main.js
@@ -3,4 +3,9 @@
 //It might be a link to something we are passionate about
 
 //Or it might be a button that performs an operation, i.e. performing a task
+function picture(){ 
+    var pic = "assets/granFun.jpg"
+    document.getElementById('fun').src = pic.replace('90x90', '225x225');
+    document.getElementById('fun').style.display='block';
 
+    }
